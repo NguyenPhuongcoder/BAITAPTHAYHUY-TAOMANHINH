4891044f5376fb76bf9c43113978e47610130c4b
NguyenPhuongCoder <182530477+NguyenPhuongcoder@users.noreply.github.com>
2025-09-24 01:21:04 +0700

    handle move page

--- app/src/main/java/com/example/btthayhuy_giaodienuser/MainActivity.java
+++ app/src/main/java/com/example/btthayhuy_giaodienuser/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.btthayhuy_giaodienuser;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,22 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button btnLogin = findViewById(R.id.button);
+        EditText edtEmail = findViewById(R.id.editTextTextEmailAddress);
+        EditText edtPassword = findViewById(R.id.editTextTextPassword);
+        btnLogin.setOnClickListener(v -> {
+            String email = edtEmail.getText().toString().trim();
+            String password = edtPassword.getText().toString();
+            // Thông tin mặc định
+            String defaultEmail = "user@gmail.com";
+            String defaultPassword = "123456";
+            if(email.equals(defaultEmail) && password.equals(defaultPassword)) {
+                Intent intent = new Intent(MainActivity.this, UserProfile.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(MainActivity.this, "Email hoặc mật khẩu không đúng!", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
